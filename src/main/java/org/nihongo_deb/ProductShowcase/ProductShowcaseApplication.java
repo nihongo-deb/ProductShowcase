d96583345bbddc660a425cb4f86a81963c564bae
nihongo-deb <dal34@tpu.ru>
2023-08-16 12:44:16 +0700

    start project with configured db schema

---
+++ src/main/java/org/nihongo_deb/ProductShowcase/ProductShowcaseApplication.java
@@ -0,0 +1,13 @@
+package org.nihongo_deb.ProductShowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nihongo_deb/ProductShowcase/ProductShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nihongo_deb.ProductShowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
