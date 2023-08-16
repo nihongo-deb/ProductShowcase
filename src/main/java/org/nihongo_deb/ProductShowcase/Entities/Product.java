f1a6004f1df24d4a46c98d42773a6508cfe06b45
nihongo-deb <dal34@tpu.ru>
2023-08-16 13:34:29 +0700

    create entities

---
+++ src/main/java/org/nihongo_deb/ProductShowcase/Entities/Product.java
@@ -0,0 +1,106 @@
+package org.nihongo_deb.ProductShowcase.Entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * @author KAWAIISHY
+ * @project ProductShowcase
+ * @created 16.08.2023
+ */
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID uuid;
+
+    @Column(name = "product_position")
+    private Integer position;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "product_type")
+    private String type;
+
+    @Column(name = "product_price")
+    private Double price;
+
+    @Column(name = "product_created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "product_updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "showcase_id", referencedColumnName = "showcase_id")
+    private Showcase owner;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Showcase getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Showcase owner) {
+        this.owner = owner;
+    }
+}
---
+++ src/main/java/org/nihongo_deb/ProductShowcase/Entities/Showcase.java
@@ -0,0 +1,97 @@
+package org.nihongo_deb.ProductShowcase.Entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author KAWAIISHY
+ * @project ProductShowcase
+ * @created 16.08.2023
+ */
+
+@Entity
+@Table(name = "showcase")
+public class Showcase {
+    @Id
+    @Column(name = "showcase_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID uuid;
+
+    @Column(name = "showcase_name")
+    private String name;
+
+    @Column(name = "showcase_address")
+    private String address;
+
+    @Column(name = "showcase_type")
+    private String type; // TODO change to enum
+
+    @Column(name = "showcase_created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "showcase_updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Product> products;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
